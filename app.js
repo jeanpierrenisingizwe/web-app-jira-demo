552a750edc015037cab75e6ba3ad3f32799ee372
Peter <jeanpierrenisingizwe5@gmail.com>
2025-11-16 23:18:05 +0200

    WEB-1: Update welcome home message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Homepage');
+  res.send('Welcome to Homepage');
 });
 
 app.listen(PORT, () => {
